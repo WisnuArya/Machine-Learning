680845939f9d8eed5b009f751cf1de9549ae9f51
WisnuArya <60162507+WisnuArya@users.noreply.github.com>
2024-03-25 17:31:42 +0700

    Add files via upload

---
+++ Minggu 2/LatihanInputOutput.py
@@ -0,0 +1,10 @@
+#Input Ouput Sederhana
+nama = input("Masukkan Nama         :")
+nim = input("Masukkan NIM           :")
+kelompok = input("Masukkan Kelompok :")
+matkul = input("Masukkan Matkul     : ")
+
+print("Nama     :",nama)
+print("NIM      :",nim)
+print("Kelompok :",kelompok)
+print("Matkul   :",matkul)
---
+++ Minggu 2/LatihanPerhitungan.py
@@ -0,0 +1,29 @@
+#Penjumlahan
+print("Penjumlahan")
+tambah1 = float(input("Masukan Angka Ke-1 : "))
+tambah2 = float(input("Masukan Angka Ke-2 : "))
+print("Hasil jumlah : ", tambah1+tambah2)
+
+#Pengurangan
+print("\nPengurangan")
+kurang1 = float(input("Masukan Angka Ke-1 : "))
+kurang2 = float(input("Masukan Angka Ke-2 : "))
+print("Hasil kurang : ", kurang1-kurang2)
+
+#Perkalian
+print("\nPerkalian")
+kali1 = float(input("Masukan Angka Ke-1 : "))
+kali2 = float(input("Masukan Angka Ke-2 : "))
+print("Hasil kali   : ", kali1*kali2)
+
+#Pembagian
+print("\nPembagian")
+bagi1 = float(input("Masukan Angka Ke-1 : "))
+bagi2 = float(input("Masukan Angka Ke-2 : "))
+print("Hasil bagi   : ", bagi1/bagi2)
+
+#Pangkat
+print("\nPangkat")
+angka = float(input("Masukan Angka      : "))
+pangkat = float(input("Masukan Pangkat    : "))
+print("Pangkat duanya adalah ", angka**pangkat)
